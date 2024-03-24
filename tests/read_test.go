5c70509244ae35f160fb12449284dd7aeecc3549
Pawel <screamingbox@gmail.com>
2024-03-24 23:54:23 +0100

    Save

---
+++ tests/read_test.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	gofilemirror "github.com/skazanyNaGlany/go-file-mirror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRead(t *testing.T) {
+	fm := gofilemirror.NewFileMirror()
+
+	f, err := fm.CreateTemp("/tmp", "testing_file_mirror")
+	if err != nil {
+		panic(err)
+	}
+
+	f2, err := fm.CreateTemp("/tmp", "testing_file_mirror")
+	if err != nil {
+		panic(err)
+	}
+
+	// read at 0 position
+	strb := []byte("123456abc")
+	readed := make([]byte, 6)
+
+	n, err := f.Read(readed)
+	assert.NotNil(t, err)
+	assert.Zero(t, n)
+
+	n, err = f2.Read(readed)
+	assert.NotNil(t, err)
+	assert.Zero(t, n)
+
+	n, err = f2.Write(strb)
+	assert.Nil(t, err)
+	assert.Equal(t, len(strb), n)
+
+	// cannot read at EOF
+	n, err = f.Read(readed)
+	assert.NotNil(t, err)
+	assert.Zero(t, n)
+
+	ret, err := f.Seek(4, io.SeekStart)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(4), ret)
+
+	n, err = f.Read(readed)
+	assert.Nil(t, err)
+	assert.Equal(t, n, 5)
+
+	ret, err = f2.Seek(5, io.SeekStart)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(5), ret)
+
+	n, err = f2.Read(readed)
+	assert.Nil(t, err)
+	assert.Equal(t, n, 4)
+
+	err = f.Close()
+	assert.Nil(t, err)
+
+	// all files within that FileMirror instance
+	// have been closed, calling Close() again
+	// should return an error
+	err = f2.Close()
+	assert.NotNil(t, err)
+	assert.ErrorAs(t, err, &os.ErrClosed)
+}
---
+++ tests/write_test.go
@@ -0,0 +1,79 @@
+package tests
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	gofilemirror "github.com/skazanyNaGlany/go-file-mirror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWrite(t *testing.T) {
+	fm := gofilemirror.NewFileMirror()
+
+	f, err := fm.CreateTemp("/tmp", "testing_file_mirror")
+	if err != nil {
+		panic(err)
+	}
+
+	f2, err := fm.CreateTemp("/tmp", "testing_file_mirror")
+	if err != nil {
+		panic(err)
+	}
+
+	// write string and try to read it at 0 position
+	strb := []byte("123abc")
+	readed := make([]byte, len(strb))
+
+	n, err := f.Write(strb)
+	assert.Nil(t, err)
+	assert.Equal(t, len(strb), n)
+
+	ret, err := f.Seek(0, io.SeekStart)
+	assert.Nil(t, err)
+	assert.Zero(t, ret)
+
+	n, err = f.Read(readed)
+	assert.Nil(t, err)
+	assert.Equal(t, len(strb), n)
+
+	// write other string at 2 position
+	strb2 := []byte("defghi")
+	readed = make([]byte, len(strb2))
+
+	ret, err = f2.Seek(2, io.SeekStart)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(2), ret)
+
+	n, err = f2.Write(strb2)
+	assert.Nil(t, err)
+	assert.Equal(t, len(strb), n)
+
+	ret, err = f2.Seek(2, io.SeekStart)
+	assert.Nil(t, err)
+	assert.Equal(t, int64(2), ret)
+
+	n, err = f.Read(readed)
+	assert.Nil(t, err)
+	assert.Equal(t, len(strb2), n)
+	assert.Equal(t, readed, strb2)
+
+	f1i, err := f.Stat()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(8), f1i.Size())
+
+	f2i, err := f.Stat()
+	assert.Nil(t, err)
+	assert.Equal(t, f1i.Size(), f2i.Size())
+
+	err = f.Close()
+	assert.Nil(t, err)
+
+	// all files within that FileMirror instance
+	// have been closed, calling Close() again
+	// should return an error
+	err = f2.Close()
+	assert.NotNil(t, err)
+	assert.ErrorAs(t, err, &os.ErrClosed)
+}
