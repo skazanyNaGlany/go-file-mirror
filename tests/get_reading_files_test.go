fe0beb75c44d915594fc8d7fa4bfab14254d4509
Pawel <screamingbox@gmail.com>
2024-03-26 23:48:04 +0100

    Save

---
+++ tests/get_reading_files_test.go
@@ -0,0 +1,37 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	gofilemirror "github.com/skazanyNaGlany/go-file-mirror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetReadingFiles(t *testing.T) {
+	fm := gofilemirror.NewFileMirror()
+
+	f, err := fm.CreateTemp("/tmp", "testing_file_mirror")
+	if err != nil {
+		panic(err)
+	}
+
+	f2, err := fm.CreateTemp("/tmp", "testing_file_mirror")
+	if err != nil {
+		panic(err)
+	}
+
+	files := fm.GetReadingFiles()
+	assert.Contains(t, files, f)
+	assert.Contains(t, files, f2)
+
+	err = f.Close()
+	assert.Nil(t, err)
+
+	// all files within that FileMirror instance
+	// have been closed, calling Close() again
+	// should return an error
+	err = f2.Close()
+	assert.NotNil(t, err)
+	assert.ErrorAs(t, err, &os.ErrClosed)
+}
