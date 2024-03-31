1b23a53fca2ac7c04bff0b6559d7b6d9bb1e45c4
Pawel <screamingbox@gmail.com>
2024-03-31 08:51:26 +0200

    Save

--- errors.go
+++ errors.go
@@ -2,7 +2,6 @@ package gofilemirror
 
 import "errors"
 
-var ErrDoNotBelong = errors.New("file does not belong to FileMirror instance")
 var ErrNoFilesToRead = errors.New("no files to read")
 var ErrNoFilesToWrite = errors.New("no files to write")
 var ErrNoFiles = errors.New("no files")
