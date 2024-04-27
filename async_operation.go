f8138af3938c5623a10fdb6f93e211ef495a353f
Pawel <screamingbox@gmail.com>
2024-04-28 00:21:55 +0200

    Added more setters

--- async_operation.go
+++ async_operation.go
@@ -32,44 +32,88 @@ func (ao *AsyncOperation) GetFile() *os.File {
 	return ao.file
 }
 
+func (ao *AsyncOperation) SetFile(file *os.File) {
+	ao.file = file
+}
+
 func (ao *AsyncOperation) GetType() AsyncOperationType {
 	return ao._type
+}
+
+func (ao *AsyncOperation) SetType(_type AsyncOperationType) {
+	ao._type = _type
 }
 
 func (ao *AsyncOperation) GetLastResultError() error {
 	return ao.err
 }
 
+func (ao *AsyncOperation) SetLastResultError(err error) {
+	ao.err = err
+}
+
 func (ao *AsyncOperation) GetLastResultInt() int64 {
 	return ao.resultInt
+}
+
+func (ao *AsyncOperation) SetLastResultInt(resultInt int64) {
+	ao.resultInt = resultInt
 }
 
 func (ao *AsyncOperation) GetBuffer() []byte {
 	return ao.buffer
 }
 
+func (ao *AsyncOperation) SetBuffer(buffer []byte) {
+	ao.buffer = buffer
+}
+
 func (ao *AsyncOperation) GetOffset() int64 {
 	return ao.offset
+}
+
+func (ao *AsyncOperation) SetOffset(offset int64) {
+	ao.offset = offset
 }
 
 func (ao *AsyncOperation) GetWhence() int {
 	return ao.whence
 }
 
+func (ao *AsyncOperation) SetWhence(whence int) {
+	ao.whence = whence
+}
+
 func (ao *AsyncOperation) GetSize() int64 {
 	return ao.size
+}
+
+func (ao *AsyncOperation) SetSize(size int64) {
+	ao.size = size
 }
 
 func (ao *AsyncOperation) GetStringBuffer() string {
 	return ao.stringBuffer
 }
 
+func (ao *AsyncOperation) SetStringBuffer(stringBuffer string) {
+	ao.stringBuffer = stringBuffer
+}
+
 func (ao *AsyncOperation) IsStarted() bool {
 	return ao.started
 }
 
+func (ao *AsyncOperation) SetStarted(started bool) {
+	ao.started = started
+}
+
 func (ao *AsyncOperation) IsDone() bool {
 	return ao.done
+}
+
+func (ao *AsyncOperation) SetDone(started bool) {
+	ao.started = started
 }
 
 func (ao *AsyncOperation) WaitForStart(duration time.Duration) {
